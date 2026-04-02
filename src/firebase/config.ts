f22147dffab593cbb4f46f9dbd8eec0f57f0b8ab
Ezekiel Ojochenemi <ezekielojochenemi28@gmail.com>
2026-04-02 23:19:19 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-3126553432-7333f",
+  "appId": "1:149946455527:web:5ba32320e73e6a4ed51942",
+  "apiKey": "AIzaSyCGFJ6Mqgu3bgWC4PBmgc9ekW96ZjHFCeU",
+  "authDomain": "studio-3126553432-7333f.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "149946455527"
+};
