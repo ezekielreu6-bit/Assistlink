399bb6d2a55b64a0272b9c26498d2e9d2979d20f
ezekielreu6-bit <ezekielreu6@gmail.com>
2026-04-03 15:17:27 +0100

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -15,6 +15,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+       {
+        protocol: 'https',
+        hostname: 'ik.imagekit.io',
+        port: '',
+        pathname: '/**',
+      },
       {
         protocol: 'https',
         hostname: 'images.unsplash.com',
